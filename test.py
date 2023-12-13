57fc417a9543516db046448ce175b2e4f2d9c194
Navjeevan <jeevanchy@gmail.com>
2023-12-13 17:14:08 +0530

    Add class

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-print("Hello")
+class Car:
+    def __init__(self, model='', year=0):
+        self.model = model
+        self.year = year
+        self.speed = 0
+    
+    def saveCar(self, model, year):
+        self.model = model
+        self.year = year
+    def increseSpeed(self, accelerate):
+        if(accelerate>0):
+            print(f'speed increased by {accelerate}')
+        else:
+            print(f'Invalid input')
+        self.speed = accelerate
 
 
-my_string = "I've done nothing today's work"
+car = Car()
+print(car.saveCar('Toyota',2003))
+car.increseSpeed(23)
+print(car.speed)
+print(car.model)
+
